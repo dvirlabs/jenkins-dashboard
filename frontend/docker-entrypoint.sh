b2c738736ac06f449b6abe18fc65b127a0f4c851
dvirlabs <dvirlabs@gmail.com>
2025-02-23 03:42:46 +0200

    Copy env.js file

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # החלפת משתני סביבה בקובץ env.js.template
-envsubst < /usr/share/nginx/html/env.js.template > /usr/share/nginx/html/env.js
+envsubst < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
 
 # הרצת nginx
 exec "$@"
